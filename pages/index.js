9d885c43a29b387f80534f2c7bf1cb237034e4f3
Lucas Leandro <lucasfariasleandro@gmail.com>
2021-01-26 23:27:22 -0300

    Conteudo da primeira aula imersao

---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Em que ano o corinthians foi fundado?</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <ul>
+              <li>a) 1932</li>
+              <li>b) 1945</li>
+              <li>c) 1910</li>
+              <li>d) 1920</li>
+            </ul>         
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+        <Widget.Header>
+            <h1>  Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <h1></h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  );
+}
